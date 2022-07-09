f2329aa2e90a8c9911ba8ba7a337dd637193ac40
LWolf-13 <108947339+LWolf-13@users.noreply.github.com>
2022-07-09 18:58:06 +0200

    Add files via upload

---
+++ UD1 - Valor/UD1 - Valor/Program.cs
@@ -0,0 +1,26 @@
+﻿// See https://aka.ms/new-console-template for more information
+namespace ConsoleApplications5
+{
+
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+
+            int num1 = 5;
+            int num2 = 6;
+            double numero3 = 2.57;
+            double suma;
+            suma = num1 + numero3;
+
+
+            Console.WriteLine(suma);
+            string valor1 = Console.ReadLine();
+            Console.WriteLine("El numero introducido es: " + valor1);
+            int valorint = Convert.ToInt32(valor1);
+
+            Console.ReadKey();
+        }
+    }
+}
